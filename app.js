503861dc559ecd892de18568529ffd2be0bbd07d
KryptoGhost <106913123+KryptoGhost@users.noreply.github.com>
2022-12-04 23:19:30 +0100

    commit change

--- app.js
+++ app.js
@@ -2,32 +2,26 @@ const btns = document.querySelectorAll(".tab-btn");
 const article = document.querySelectorAll(".content");
 const about = document.querySelector(".about");
 
-btns.forEach((btn) => {
+/*btns.forEach((btn) => {
     btn.addEventListener("click", function(e) {
-        const tap = e.currentTarget.dataset.id;
-        if (tap) {
-            tap.classList.remove("active");
-            e.currentTarget.classList.add("active");
-            article.forEach((art) => {
-                art.classList.remove("active");
-            })
-            const element = document.getElementById(tap);
-            element.classList.add("active");
-        }
+        const id = e.currentTarget.dataset;
+        console.log(id);
     })
-})
+});*/
 
-/*about.addEventListener("click", function(e) {
-    const id = e.target.dataset.id;
+about.addEventListener("click", function(e) {
+    const id = e.target.dataset.id; //tagetet the buttons with their data attribute
+    console.log(id);
     if (id) {
         btns.forEach((btn) => {
-            btn.classList.remove("active");
+            btn.classList.remove("active"); // remove the active class
         });
-        e.target.classList.add("active");
+        e.target.classList.add("active"); // add active to selected button
         article.forEach(function (article) {
-            article.classList.remove("active");
+            article.classList.remove("active"); //remove the active class
         });
-        const element = document.getElementById(id);
+        //target any aspect with the id name 
+        const element = document.getElementById(id); 
         element.classList.add("active");
     }
-})
+});
